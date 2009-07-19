769f17b5e8dac80fdd15d4827264e08733d52970
kirill.lastovirya <kirill.lastovirya@a4d82d58-7453-11de-b168-715220e24c25>
2009-07-19 13:15:55 +0000

    client s done

--- trunk/vkontakte-backend/src/org/gpmedia/vkontakte/client/VkontakteBackendTestClient.java
+++ trunk/vkontakte-backend/src/org/gpmedia/vkontakte/client/VkontakteBackendTestClient.java
@@ -4,17 +4,34 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.HttpUnitOptions;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+
 public class VkontakteBackendTestClient {
 	
 	@Option(name="-url",usage="Server to call")
-	private final static String url = null;
+	private final String url = null;
 	
 	
 	public static void main(String[] args) {
 		System.out.println("Starting Vkontakte backend");
 		VkontakteBackendTestClient client = parseParameters(args);
-		System.out.println("Calling url: " + url);
+		System.out.println("Calling url: " + client.url);
 		
+		WebConversation conversation = new WebConversation ();
+		HttpUnitOptions.setScriptingEnabled(false);
+		WebRequest request = new GetMethodWebRequest (client.url);
+		WebResponse response = null;
+		try {
+			response = conversation.sendRequest(request );
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Cannot send request");
+		} 
+		System.out.println("Received responce: " + response);
 	}
 
 
@@ -29,6 +46,11 @@ public class VkontakteBackendTestClient {
             // handling of wrong arguments
             System.err.println(e.getMessage());
             parser.printUsage(System.err);
+        }
+        //TODO understand how to use Arguments
+        if (client.url == null) {
+        	System.err.println("URL should be specified");
+        	System.exit(1);
         }
         return client;
 	}
